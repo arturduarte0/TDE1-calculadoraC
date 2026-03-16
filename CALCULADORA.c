02ca6795c0a7ccef615d6fe714514fa0c5a5edfc
arturduarte0 <arturduate247@gmail.com>
2026-03-16 18:13:21 -0300

    Update CALCULADORA.c

--- CALCULADORA.c
+++ CALCULADORA.c
@@ -1,14 +1,65 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, resultado;
 
-    printf("Digite dois numeros: ");
-    scanf("%d %d", &a, &b);
+    int opcao;
+    float a, b, resultado;
 
-    resultado = a + b;
+    do {
+        printf("\n=== CALCULADORA ===\n");
+        printf("1 - Somar\n");
+        printf("2 - Subtrair\n");
+        printf("3 - Multiplicar\n");
+        printf("4 - Dividir\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
 
-    printf("Resultado da soma: %d\n", resultado);
+        switch(opcao) {
+
+            case 1:
+                printf("Digite dois numeros: ");
+                scanf("%f %f", &a, &b);
+                resultado = a + b;
+                printf("Resultado: %.2f\n", resultado);
+                break;
+
+            case 2:
+                printf("Digite dois numeros: ");
+                scanf("%f %f", &a, &b);
+                resultado = a - b;
+                printf("Resultado: %.2f\n", resultado);
+                break;
+
+            case 3:
+                printf("Digite dois numeros: ");
+                scanf("%f %f", &a, &b);
+                resultado = a * b;
+                printf("Resultado: %.2f\n", resultado);
+                break;
+
+            case 4:
+                printf("Digite dois numeros: ");
+                scanf("%f %f", &a, &b);
+
+                if(b != 0) {
+                    resultado = a / b;
+                    printf("Resultado: %.2f\n", resultado);
+                } else {
+                    printf("Erro: divisao por zero!\n");
+                }
+                break;
+
+            case 0:
+                printf("Encerrando...\n");
+                break;
+
+            default:
+                printf("Opcao invalida!\n");
+
+        }
+
+    } while(opcao != 0);
 
     return 0;
 }
